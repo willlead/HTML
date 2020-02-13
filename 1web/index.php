308057cdd9049547ec788cbc83a0e9e05a5d32e6
willlead <willlead@naver.com>
2020-02-13 19:05:49 +0900

    헤더 작업 중

--- 1web/index.php
+++ 1web/index.php
@@ -2,9 +2,26 @@
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
-    <title>MyRecipe - main</title>
+    <title>MyRecipe - main</title>    
+    <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="css/common.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <style >
+
+    </style>
 </head>
 <body>
-    <p>처음  시작입니다.</p>
+   <div class="top">
+       <img src="images/22.jpg" alt="">
+       <p>MY RECIPE</p>
+   </div>
+    <div class="nav">
+       <ul>
+        <li><a href="#">홈</a></li>
+        <li><a href="#">인기 레시피</a></li>
+        <li><a href="#">게시판</a></li>
+       </ul>
+        
+    </div>
 </body>
 </html>
